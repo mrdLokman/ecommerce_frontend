fd9df0321860030f7db5b9532b8ef2f42638d56e
mrdLokman <merdjilokman@gmail.com>
2022-05-15 11:53:41 -0700

    finish single product page

--- web_app/src/pages/index.jsx
+++ web_app/src/pages/index.jsx
@@ -1,5 +1,7 @@
 import Home from "./Home";
 import ProductsPage from "./ProductsPage";
 import SingleProductPage from "./SingleProductPage"
+import Register from "./Register";
+import Login from "./Login";
 
-export {Home, ProductsPage, SingleProductPage};
+export { Home, ProductsPage, SingleProductPage, Register, Login };
